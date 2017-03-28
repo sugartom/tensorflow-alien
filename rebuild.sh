387e365f48a993a1d51b69d6962b3138192b16f8
sugartom <sugartom.hu@gmail.com>
2017-03-28 13:44:30 -0700

    make tensorflow tensorflow-alien

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+sudo pip uninstall -y protobuf
+sudo pip uninstall -y tensorflow
+bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip install /tmp/tensorflow_pkg/tensorflow-1.0.1-cp27-none-linux_x86_64.whl
