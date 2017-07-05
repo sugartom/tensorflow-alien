bb7b5a3c229b964ffc71a9b70e89c6c13586fe1a
sugartom <sugartom.hu@gmail.com>
2017-07-05 14:45:31 -0700

    code backup for previous offline scheduling where we need to know the whole graph, and TLS is using a naive scheduling method

---
+++ reinstall-original-tf.sh
@@ -0,0 +1,3 @@
+sudo pip uninstall -y protobuf
+sudo pip uninstall -y tensorflow
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.1.0-cp27-none-linux_x86_64.whl
